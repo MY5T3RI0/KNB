d40d164959fe1693470f31db48f83ff4b1b92332
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-09-18 21:02:22 +0300

    Создана главная страница

--- KNB/App_Start/BundleConfig.cs
+++ KNB/App_Start/BundleConfig.cs
@@ -8,9 +8,23 @@ namespace KNB.App_Start
 {
     public class BundleConfig
     {
-        public static void RegisterBundles(BundleCollection bundles)
+        public static void RegisterBundles(BundleCollection bundles) //System.Web.Optimization lib
         {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js")); //jquery lib
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.validate.unobtrusive*", //jquery.unobtrussive lib
+                "~/Scripts/jquery.validate*"));   //jquery.validate lib
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/respond.js")); //respond lib
+
+            bundles.Add(new ScriptBundle("~/Content/css").Include(
+                "~/Content/Site.css"));                     //создание адресов
         }
     }
 }
