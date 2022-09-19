431107094a91e16ddb4f68b8436bd741efead3e3
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-09-19 15:06:18 +0300

    Добавлены страницы "Контакты" и "О нас"

--- KNB/Controllers/HomeController.cs
+++ KNB/Controllers/HomeController.cs
@@ -13,5 +13,15 @@ namespace KNB.Controllers
         {
             return View();
         }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
     }
 }
